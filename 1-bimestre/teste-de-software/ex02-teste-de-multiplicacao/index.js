8069264fdfc1c95e111caf6d0f8d26a0c35fd00b
✧ Sophia Cordeiro✧ <emilisophiassss@gmail.com>
2026-02-23 11:48:59 +0000

    pronto

---
+++ 1-bimestre/teste-de-software/ex02-teste-de-multiplicacao/index.js
@@ -0,0 +1,5 @@
+function multiplicar(a, b){
+   return(2 * 3)
+}
+
+module.exports = multiplicar
---
+++ 1-bimestre/teste-de-software/ex02-teste-de-multiplicacao/test/index.test.js
@@ -0,0 +1,5 @@
+test("2 x 3 deve ser 6", () => {
+    expect(2 * 3).toBe(6)
+    })
+
+    require("../index")
